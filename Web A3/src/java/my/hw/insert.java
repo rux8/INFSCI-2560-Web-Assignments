7977ba99f2f108607cbd70d806ecfd1fb549a9f9
rux8 <rux8@pitt.edu>
2018-11-05 15:29:35 -0500

    Add files via upload

---
+++ Web A3/src/java/my/hw/insert.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.hw;
+
+/**
+ *
+ * @author coco
+ */
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class insert extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    try{
+    Class.forName("org.apache.derby.jdbc.ClientDriver");
+    String url="jdbc:derby://localhost:1527/Ke Yan";
+    Connection con= DriverManager.getConnection(url,"IS2560","IS2560");
+    
+    PreparedStatement s;
+        s = con.prepareStatement("insert into mytable (student,course,id) "
+                + "values(?,?,?)");
+    
+    s.setString(1,request.getParameter("student"));
+    
+    s.setString(2,request.getParameter("course"));
+    
+    s.setString(3,request.getParameter("id"));
+    System.out.println(s);
+    s.executeUpdate();
+    result result= new result();
+    result.setStudent(request.getParameter("student"));
+    result.setCourse(request.getParameter("course"));
+    result.setId(Integer.parseInt(request.getParameter("id")));
+
+    HttpSession session= request.getSession(true);
+    session.setAttribute("myBean", result);
+    RequestDispatcher rd= getServletContext().getRequestDispatcher("/display.jsp");
+    rd.forward(request,response);
+}catch(ClassNotFoundException cnfe) {cnfe.printStackTrace();
+}catch(SQLException se){se.printStackTrace();}
+    }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
---
+++ Web A3/src/java/my/hw/result.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.hw;
+
+/**
+ *
+ * @author coco
+ */
+public class result {
+    
+    private String student;
+    private String course;
+    private int id;
+
+    /**
+     * @return the student
+     */
+    public String getStudent() {
+        return student;
+    }
+
+    /**
+     * @param student the student to set
+     */
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    /**
+     * @return the course
+     */
+    public String getCourse() {
+        return course;
+    }
+
+    /**
+     * @param course the course to set
+     */
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
